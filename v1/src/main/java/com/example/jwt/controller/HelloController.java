720a5ab5259fdf68f6d72a2dc3cd64c54831e0c3
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-30 14:19:51 -0300

    Update HelloController.java

--- v1/src/main/java/com/example/jwt/controller/HelloController.java
+++ v1/src/main/java/com/example/jwt/controller/HelloController.java
@@ -1,7 +1,5 @@
 package com.example.jwt.controller;
 
-
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
